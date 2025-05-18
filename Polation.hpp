91de46b4c90a87bc24dde245d74a42056e4c3405
siddharthk004 <siddharthkardile5@gmail.com>
2025-05-18 12:00:00 +0530

    new Final Code For Lab

--- Polation.hpp
+++ Polation.hpp
@@ -7,10 +7,15 @@ using namespace std;
 class Polation
 {
 public:
-    double lagrangeInterpolation(double x,Matrix EQ1,Matrix EQ2);
+    double lagrangeInterpolation(double x, Matrix EQ1, Matrix EQ2);
     // double PowerFit();
     double lagrangeI(double x);
     // double ExponentialFit();
     double lineFunc(double xi, double a, double b, double c);
-    double LineParabola(Matrix x,Matrix y);
+    double LineParabola(Matrix x, Matrix y);
+    double PowerFit(Matrix x, Matrix y);
+    double ExponentialFit(Matrix x, Matrix y);
+    double Tchebyshev(int n, double x);
+
+    
 };
